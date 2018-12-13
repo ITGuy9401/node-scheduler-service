e1a885242e11b6a121bb52df4555b9f2b89b48c1
Arcangelo Vicedomini <arcangelo.vicedomini@nctechimaging.com>
2018-12-13 11:08:11 +0000

    dev

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import logger from "./logger";
 
 const PATH = path.join(__dirname, "..", "..", "jobs-enabled");
 
@@ -7,6 +8,7 @@ const jobs = fs.readdirSync(PATH, {
     withFileTypes: true
 }).filter(file => file.name.toString().endsWith(".json"))
     .map(file => {
+        logger.info(`logging file ${file.name}`);
         if (file.isFile()) {
             return fs.readFileSync(path.join(PATH, file.name), {
                 encoding: "utf8"
