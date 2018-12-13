9c6df372001d97de63b28b9d39949670be94d7fe
Arcangelo Vicedomini <arcangelo.vicedomini@nctechimaging.com>
2018-12-13 09:57:43 +0000

    dev

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,10 @@ const PATH = path.join(__dirname, "..", "..", "jobs-enabled");
 
 const jobs = fs.readdirSync(PATH, {
     withFileTypes: true
-}).filter(file => file.name.toString().endsWith(".json"))
+}).filter(file => {
+    console.log(file);
+    return file.name.toString().endsWith(".json");
+})
     .map(file => {
         if (file.isFile()) {
             return fs.readFileSync(path.join(PATH, file.name), {
